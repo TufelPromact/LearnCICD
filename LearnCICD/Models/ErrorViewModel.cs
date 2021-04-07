b56a8ed095274202de3828fb9381c7b3e15b8637
tufel@promactinfo.com <tufel@promactinfo.com>
2021-04-07 11:01:08 +0530

    Initial Commit

---
+++ LearnCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearnCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
